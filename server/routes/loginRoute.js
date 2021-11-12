ab2594fa0804fdc376388b732aba16301256f962
Alleny244 <alleny244@gmail.com>
2021-11-13 02:15:59 +0530

    Login and registration done

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,7 +1,27 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const UserModel = require("../models/user");
+const router = express.Router();
 
-router.post("/",(req,res)=>{
-
+router.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(password);
+    const user = await UserModel.findOne({
+      email: email,
+    });
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        return res.status(200).send("Login Success");
+      } else {
+        return res.status(403).send("Incorrect Password");
+      }
+    } else {
+      return res.status(401).send("Email Not Registered !!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
-module.exports=router;
+module.exports = router;
--- server/routes/registerRoutes.js
+++ server/routes/registerRoutes.js
@@ -1,7 +1,30 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const UserModel = require("../models/user");
+const router = express.Router();
 
-router.post("/",(req,res)=>{
-
+router.post("/", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt());
+    const user = new UserModel({
+      name,
+      email,
+      password:hashedPassword,
+    });
+    user
+      .save()
+      .then((resp) => {
+        console.log("User registered sucessfully");
+        return res.status(200).send("User registered sucessfully");
+      })
+      .catch((error) => {
+        if (error.keyValue.email != null && error.code === 11000) {
+          return res.status(422).send("Email already registered !!!");
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
 });
-module.exports=router;
+module.exports = router;
