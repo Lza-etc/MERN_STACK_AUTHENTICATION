692435d781f35f69940ab38e382636e0aa6891ac
Alleny244 <alleny244@gmail.com>
2021-11-17 00:10:34 +0530

    changed allowed origin url in server side

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app=express();
 const PORT=process.env.PORT || 5000;
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://tender-poincare-56ebfe.netlify.app",
 }));
 
 app.use(express.json());
