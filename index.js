028a45bf90c7620aa76e5a3fc7d4548e02a3a6bc
Alleny244 <alleny244@gmail.com>
2021-11-20 18:39:10 +0530

    Added domain to url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app=express();
 const PORT=process.env.PORT || 5000;
 
 app.use(cors({
-    origin:"https://tender-poincare-56ebfe.netlify.app",
+    origin:"https://allen.software/",
 }));
 
 app.use(express.json());
